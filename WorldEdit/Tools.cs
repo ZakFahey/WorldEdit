7b0c81db71059d5f665f58d1bc44c92110d8f429
ZakFahey <z.fahey@gmail.com>
2025-05-15 19:03:06 -0700

    Update TShock to the latest version (#19)
    https://github.com/Pryaxis/TShock/releases/tag/v5.2.4

--- WorldEdit/Tools.cs
+++ WorldEdit/Tools.cs
@@ -4,13 +4,13 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using Terraria;
+using Terraria.ID;
 using TShockAPI;
 using TShockAPI.DB;
 using Terraria.GameContent.Tile_Entities;
 using Terraria.DataStructures;
 using Microsoft.Xna.Framework;
 using WorldEdit.Commands;
-using System.Threading;
 
 namespace WorldEdit
 {
@@ -52,7 +52,7 @@ namespace WorldEdit
         }
         public static List<TilePlaceID> GetTileID(string tile)
         {
-            if (int.TryParse(tile, out int ID) && ID >= 0 && ID < Main.maxTileSets)
+            if (int.TryParse(tile, out int ID) && ID >= 0 && ID < TileID.Count)
                 return new() { new BlockPlaceID(ID, -1, tile) };
 
             if (tile.Contains("/") && tile.Split("/").Count() == 2)
@@ -60,7 +60,7 @@ namespace WorldEdit
                 var arr = tile.Split("/");
 
                 if (
-                    int.TryParse(arr[0], out int ID1) && ID1 >= 0 && ID1 < Main.maxTileSets
+                    int.TryParse(arr[0], out int ID1) && ID1 >= 0 && ID1 < TileID.Count
                     &&
                     int.TryParse(arr[1], out int ID2) && ID2 >= 0
                 )
@@ -82,7 +82,7 @@ namespace WorldEdit
         public static List<WallPlaceID> GetWallID(string wall)
         {
             int ID;
-            if (int.TryParse(wall, out ID) && ID >= 0 && ID < Main.maxWallTypes)
+            if (int.TryParse(wall, out ID) && ID >= 0 && ID < WallID.Count)
                 return new() { new(ID, wall) };
 
             var list = new List<WallPlaceID>();
--- WorldEdit/WorldEdit.cs
+++ WorldEdit/WorldEdit.cs
@@ -592,7 +592,7 @@ namespace WorldEdit
 
 			Main.player[Main.myPlayer] = new Player();
 			var item = new Item();
-			for (var i = 1; i < Main.maxItemTypes; i++)
+			for (var i = 1; i < ItemID.Count; i++)
 			{
 				item.netDefaults(i);
 
@@ -667,7 +667,7 @@ namespace WorldEdit
 
 			foreach (var fi in typeof(WallID).GetFields())
 			{
-				if (!(fi.GetValue(null) is ushort id) || (id < 0) || (id >= Main.maxWallTypes))
+				if (!(fi.GetValue(null) is ushort id) || (id < 0) || (id >= WallID.Count))
 					continue;
 				string name = fi.Name;
 				var sb = new StringBuilder();
